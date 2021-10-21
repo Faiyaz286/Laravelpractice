087308ae395a0f62114a06d013c14fc2a25ae13b
Faiyaz286 <fahimfaiyaz286@gmail.com>
2021-10-21 15:23:23 +0600

    created product migration file

---
+++ database/migrations/2021_10_21_074912_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 250);
+            $table->string('slug', 250);
+            $table->text('short_description');
+            $table->longText('description');
+            $table->softDeletes('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
