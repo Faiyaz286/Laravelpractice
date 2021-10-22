96c8434bc18238aeeeeeaf6b44348d2796043b74
Faiyaz286 <fahimfaiyaz286@gmail.com>
2021-10-22 12:56:45 +0600

    created oders table

---
+++ database/migrations/2021_10_22_064847_create_oders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oders', function (Blueprint $table) {
+            $table->id();
+            $table->string('oderDate', 100);
+            $table->string('oderStatus', 100);
+            $table->softDeletes('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oders');
+    }
+}
