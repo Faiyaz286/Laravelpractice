1123d73efe85f29d42a09d925800aa63306b04f6
Faiyaz286 <fahimfaiyaz286@gmail.com>
2021-10-26 18:24:48 +0600

    Completed front page layout setup

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+
+@extends('front')
+
+@section('content')
+    <div>I am home page</div>
+@endsection
---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,67 @@
+<div class="row m-0">
+    <div class="col-md-3 col-md-offset-2 text-center">
+        <div class="py-30">
+        <div>
+            <img src="{{ asset('assets/images/constructions/logo.png') }}" alt="logo" class="logo-footer">
+        </div>
+        <h3>About</h3>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        </p>
+        </div>
+        
+    </div>
+    <div class="col-md-2 text-center">
+        <div class="py-30">
+            <ul id="footer-menu">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Shop</a>
+                </li>
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+                <li>
+                    <a href="#">Media</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+                <li>
+                    <a href="#">Login</a>
+                </li>
+                <li>
+                    <a href="#">Sign Up</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="col-md-3 text-center">
+        <div class="py-30">
+            <div class="subscription-form">
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Name*" require>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email*" require>
+                    </div>
+                    <button type="submit" class="btn btn-info btn-block">Subscribe</button>
+                </form>
+            </div>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="clear-fix"></div>
+</div>
+<div class="row m-0 bg-copyright">
+    <div class="text-center">
+        &copy; {{date('Y')}} <a href="#">Faiyazz all rights reserved.</a>
+    </div>
+</div>
