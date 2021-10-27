4d8424cce0a4ddf2fb91221da434ae21c7adb39b
Faiyaz286 <fahimfaiyaz286@gmail.com>
2021-10-27 17:31:17 +0600

    Work in product details page

---
+++ resources/views/pages/productdetails.blade.php
@@ -0,0 +1,45 @@
+@extends('front')
+
+@section('content')
+<a class="navbar-brand" href="#">
+                <img src="{{ asset('assets/images/constructions/R15.png') }}" alt="Photo" class="R15">
+                
+              </a>
+
+              <div class="col-md-3">
+        <div class="py-40">
+            <ul id="details-menu">
+                <li>
+                  <b>Product Name: R15</b> 
+                </li>
+                <li>
+         <b>Cc: 149</b> 
+                </li>
+                <li>
+                   <b>Region: India </b>
+                </li>
+                <li>
+                   <b> Model: 2021 </b>
+                </li>
+                <li>
+                   <b> Price: 4,70,000 BDT </b>
+                </li>
+
+                <button type="submit" class="btn btn-success">Buy now</button>
+                
+                <button type="submit" class="btn btn-warning">Add to cart</button>
+                
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-md-4 col-md-offset-9 m-0">
+    <div id="video-content">
+          <video width="320" height="240" controls>
+        <source src="{{ asset('assets/videos/constructions/R15.mp4') }}" type="video/mp4">
+  
+        Your browser does not support the video tag.
+       </video>
+</div>
+        </div>
+@endsection
---
+++ resources/views/pages/productlist.blade.php
@@ -0,0 +1,5 @@
+@extends('front')
+
+@section('content')
+    <div>I am product list  page</div>
+@endsection
